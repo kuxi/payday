be5c758f3fca23f2b8d390bf293a56ba1d608274
Heiðar Þórðarson <heidar@gagnavarslan.is>
2013-09-28 15:48:49 +0000

    Added local settings option

--- src/payday/settings.py
+++ src/payday/settings.py
@@ -9,3 +9,12 @@ time_tracking_login_url = 'your_url'
 time_tracking_hours_url = 'your_url'
 time_tracking_user = 'user'
 time_tracking_pass = 'pass'
+
+try:
+    import settings_local
+    time_tracking_login_url = settings_local.time_tracking_login_url
+    time_tracking_hours_url = settings_local.time_tracking_hours_url
+    time_tracking_user = settings_local.time_tracking_user
+    time_tracking_pass = settings_local.time_tracking_pass
+except ImportError:
+    pass
