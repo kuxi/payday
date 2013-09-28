8e99ba852556e275bcacc11e2da6a0032dbc5705
Heiðar Þórðarson <heidar@gagnavarslan.is>
2013-09-28 15:45:11 +0000

    Added anonymous settings file to correct branch

---
+++ src/payday/settings.py
@@ -0,0 +1,11 @@
+import os
+
+
+port = 8000
+static_path = os.path.join(os.path.dirname(__file__), 'static')
+favicon_path = os.path.join(static_path, 'img')
+
+time_tracking_login_url = 'your_url'
+time_tracking_hours_url = 'your_url'
+time_tracking_user = 'user'
+time_tracking_pass = 'pass'
